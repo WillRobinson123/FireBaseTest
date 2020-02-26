9b814f39c117fe169a5c00aaebc80cc86b4b8399
William <wdjr500@york.ac.uk>
2020-02-26 18:37:10 +0000

    Download and Account setup complete

--- app/src/main/java/com/example/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -4,20 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.firebasetest.firebase.AccountManager;
 import com.example.firebasetest.firebase.Download;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
     private Download download;
     private AccountManager accountManager;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        accountManager = new AccountManager();
-        accountManager.createAccount("pulsedpurpose@gmail.com","joebloggs",this);
-        download = new Download();
-        download.createStorageReference("pdftest.pdf");
-        download.downloadTxt(MainActivity.this, "tisworking", ".pdf");
+
         setContentView(R.layout.activity_main);
     }
 }
