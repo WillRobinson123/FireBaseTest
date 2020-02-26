ade54be47f7bc0482a093bb14fd8e486f1aec1f4
William <wdjr500@york.ac.uk>
2020-02-26 18:52:56 +0000

    Download and Account setup complete

--- app/src/main/java/com/example/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.firebasetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.example.firebasetest.firebase.AccountManager;
 import com.example.firebasetest.firebase.Download;
 import com.google.firebase.database.FirebaseDatabase;
@@ -11,11 +13,14 @@ public class MainActivity extends AppCompatActivity {
     private Download download;
     private AccountManager accountManager;
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private String text = "Done!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        toast.show();
         setContentView(R.layout.activity_main);
     }
 }
