2eb5b39e1382ec156d5d99c713f7db60bd59efb3
William <wdjr500@york.ac.uk>
2020-02-26 19:21:31 +0000

    Download and Account setup complete

--- app/src/main/java/com/example/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -1,26 +1,46 @@
 package com.example.firebasetest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.firebasetest.firebase.AccountManager;
 import com.example.firebasetest.firebase.Download;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
     private Download download;
     private AccountManager accountManager;
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private String text = "Done!";
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
-        toast.show();
+        DatabaseReference myRef = database.getReference("picture/pic_1");
+        //myRef.setValue("Please work");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                text = dataSnapshot.getValue(String.class);
+                System.out.println(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("The Read Failed");
+            }
+        });
+
         setContentView(R.layout.activity_main);
     }
 }
